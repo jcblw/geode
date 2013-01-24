88db664c12f37672d435f30d282338bd01f51727
Jacob Lowe <jacob@redeyeops.com>
2013-01-24 14:10:47 -0800

    making it work

--- geode.js
+++ geode.js
@@ -27,7 +27,7 @@ var Geode  = function(username, local){
 	};
 
 	that.merge = function(){
-		if(typeof arguments[0] === 'object' && !object[0].length){
+		if(typeof arguments[0] === 'object' && !arguments[0].length){
 			var base = arguments[0]
 			for(var i = 1; i < arguments.length; i += 1){
 				for(var key in arguments[i]){
@@ -41,13 +41,14 @@ var Geode  = function(username, local){
 	that.request = function(collection, data, callback){
 		var url = that.endpoint + collection + 'JSON';
 		var payload = that.merge(that.localize,data);
+		console.log(url);
 		request.get({
 			url : url, 
 			qs : payload
 		}, function(err, res, body){
 			if(err) that.error(err, callback);
 			else{
-				callback(null, body);
+				callback(null, JSON.parse(body));
 			}
 		});
 	};
@@ -84,16 +85,18 @@ var Geode  = function(username, local){
 		'timezone'
 	];
 
-	for(var i = 0; i < that.methods; i += 1){
+	for(var i = 0; i < that.methods.length; i += 1){
 		if(!that[that.methods[i]]){
-			that[that.methods[i]] = function(data, callback){
-				that.request(that.methods[i], data, callback);
-			};
+			(function(n){
+				that[that.methods[n]] = function(data, callback){
+					that.request(that.methods[n], data, callback);
+				};
+			}(i));
 		}
 	};
 
 };
 
-module.export = Geode;
+module.exports = Geode;
 
 
