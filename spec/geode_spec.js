b74741f0ea71ec8d9c1131a8f1244ef20393975f
Jacob Lowe <jacoblowe2.0@gmail.com>
2014-03-14 15:43:20 -0700

    swaping double quotes in spec test fixes #6

--- spec/geode_spec.js
+++ spec/geode_spec.js
@@ -1,37 +1,37 @@
-var geode   = require("../geode"),
-    should  = require("should"),
-    _       = require("underscore");
+var geode   = require('../geode'),
+    should  = require('should'),
+    _       = require('underscore');
 
-describe("#instantiate geode", function() {
+describe('#instantiate geode', function() {
 
-    it("without parameters should throw exception", function() {
+    it('without parameters should throw exception', function() {
         (function () {
             new geode();
         }).should.throw(Error);
     });
     
-    it("without'local' param should return data", function(done) {
-        var geo = new geode("demo");
+    it('without "local" param should return data', function(done) {
+        var geo = new geode('demo');
         
-        geo.search({ q: "london", maxRows: 10 }, function(err, results) {
+        geo.search({ q: 'london', maxRows: 10 }, function(err, results) {
             should.not.exist(err);
             should.exist(results);
             done();
         });        
     });
     
-    it("with 'local' param should return 'US' only data", function(done) {
+    it('with "local" param should return "US" only data', function(done) {
         var username = process.env.USER;
         if ( !username ){
             throw new Error( 'please run test with "USER={username} npm test"' );
         }
-        var geo = new geode( username , { language: "en", country : "US" });
+        var geo = new geode( username , { language: 'en', country : 'US' });
         
-        geo.search({ q: "london", maxRows: 10 }, function(err, results) {
+        geo.search({ q: 'london', maxRows: 10 }, function(err, results) {
             should.not.exist(err);
             should.exist(results);
-            results.should.have.property("geonames").with.length(10);
-            _.first(results.geonames).should.have.property("countryCode", "US");            
+            results.should.have.property('geonames').with.length(10);
+            _.first(results.geonames).should.have.property('countryCode', 'US');            
             done();
         });        
     });    
