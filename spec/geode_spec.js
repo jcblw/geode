2655d84666e353f4826dfcde6a960ff7eaddd22d
Pascal Bihler <pascal.bihler@qeevee.com>
2015-02-26 18:08:38 +0100

    Returns a GeodeError on error response from geonames.org
    This simplifies the code - either you get data, or an error.

--- spec/geode_spec.js
+++ spec/geode_spec.js
@@ -9,31 +9,47 @@ describe('#instantiate geode', function() {
             new geode();
         }).should.throw(Error);
     });
-    
+
     it('without "local" param should return data', function(done) {
-        var geo = new geode('demo');
-        
+        var username = process.env.USER;
+        if ( !username ){
+            throw new Error( 'please run test with "USER={username} npm test"' );
+        }
+        var geo = new geode(username);
+
         geo.search({ q: 'london', maxRows: 10 }, function(err, results) {
             should.not.exist(err);
             should.exist(results);
             done();
-        });        
+        });
     });
-    
+
     it('with "local" param should return "US" only data', function(done) {
         var username = process.env.USER;
         if ( !username ){
             throw new Error( 'please run test with "USER={username} npm test"' );
         }
         var geo = new geode( username , { language: 'en', country : 'US' });
-        
+
         geo.search({ q: 'london', maxRows: 10 }, function(err, results) {
             should.not.exist(err);
             should.exist(results);
             results.should.have.property('geonames').with.length(10);
-            _.first(results.geonames).should.have.property('countryCode', 'US');            
+            _.first(results.geonames).should.have.property('countryCode', 'US');
             done();
-        });        
-    });    
+        });
+    });
+
+    it('returns an GeodeError with bad username', function(done) {
+        var geo = new geode('some_weired_username_hopefully_never_exists');
+
+        geo.search({ q: 'london', maxRows: 10 }, function(err, results) {
+            should.exist(err);
+            err.should.have.property('name','GeodeError');
+            err.should.have.property('code',10);
+            err.should.have.property('message','user does not exist.');
+            done();
+        });
+    });
 
 });
