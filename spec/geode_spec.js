f5cc13d076a1065125e48d9c2e7b33d909700c0a
Pascal Bihler <pascal.bihler@qeevee.com>
2015-02-26 17:35:02 +0100

    Fixed test
    "country" should have been "countryCode"

--- spec/geode_spec.js
+++ spec/geode_spec.js
@@ -2,6 +2,11 @@ var geode   = require('../geode'),
     should  = require('should'),
     _       = require('underscore');
 
+var username = process.env.USER;
+if ( !username ){
+    throw new Error( 'please run test with "USER={username} npm test"' );
+}
+
 describe('#instantiate geode', function() {
 
     it('without parameters should throw exception', function() {
@@ -9,31 +14,29 @@ describe('#instantiate geode', function() {
             new geode();
         }).should.throw(Error);
     });
-    
+
     it('without "local" param should return data', function(done) {
-        var geo = new geode('demo');
-        
-        geo.search({ q: 'london', maxRows: 10 }, function(err, results) {
-            should.not.exist(err);
-            should.exist(results);
-            done();
-        });        
-    });
-    
-    it('with "local" param should return "US" only data', function(done) {
-        var username = process.env.USER;
-        if ( !username ){
-            throw new Error( 'please run test with "USER={username} npm test"' );
-        }
-        var geo = new geode( username , { language: 'en', country : 'US' });
-        
+        var geo = new geode(username);
+
         geo.search({ q: 'london', maxRows: 10 }, function(err, results) {
             should.not.exist(err);
             should.exist(results);
             results.should.have.property('geonames').with.length(10);
-            _.first(results.geonames).should.have.property('countryCode', 'US');            
+            _.first(results.geonames).should.have.property('name', 'London');
             done();
-        });        
-    });    
+        });
+    });
+
+    it('with "local" param should return "US" only data', function(done) {
+        var geo = new geode( username , { language: 'en', countryCode : 'US' });
+
+        geo.search({ q: 'london', maxRows: 10 }, function(err, results) {
+            should.not.exist(err);
+            should.exist(results);
+            results.should.have.property('geonames').with.length(10);
+            _.first(results.geonames).should.have.property('countryCode', 'US');
+            done();
+        });
+    });
 
 });
